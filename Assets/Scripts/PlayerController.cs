221cd07e936aa55bd778c7c622cce08f2144a41a
kingzonas <jonas.vrieling@gmail.com>
2022-05-18 07:35:08 -0400

    camera movement
    slightly modify camera movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     Vector3 movement;
+    Vector3 prevPos;
     Rigidbody rb;
     public float speed;
+    public float cameraShift= 3, cameraSpeed = 3;
     Camera cam;
     void Start()
     {
@@ -20,6 +22,8 @@ public class PlayerController : MonoBehaviour
     {
         movement = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
         rb.velocity = movement;
-        cam.transform.position = new Vector3(transform.position.x, transform.position.y + 10, transform.position.z + -15);
+        //cam.transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * cameraShift, transform.position.y + 10, transform.position.z + -15 + Input.GetAxis("Vertical") * cameraShift);
+        cam.transform.position = Vector3.Lerp(prevPos, new Vector3(transform.position.x + Input.GetAxis("Horizontal") * cameraShift, transform.position.y + 10, transform.position.z + -15 + Input.GetAxis("Vertical")), Time.deltaTime * cameraSpeed);
+        prevPos = new Vector3(transform.position.x, transform.position.y + 10, transform.position.z - 15);
     }
 }
